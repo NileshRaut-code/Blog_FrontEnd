08e1cd0b033246e22534c1b9cbb4bdfbdfc89efb
NileshRaut-code <nileshraut898@gmail.com>
2024-02-09 14:48:17 +0530

    updated header toggler

--- src/components/Loader comp/Loading.js
+++ src/components/Loader comp/Loading.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex bg-gray-700 items-center justify-center w-screen h-screen">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-blue-500 border-r-2 border-b-2 border-gray-500"></div>
+    <div className="flex dark:bg-gray-700 items-center justify-center w-screen h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2  border-r-2 border-b-2 border-gray-500"></div>
     </div>
   );
 };
